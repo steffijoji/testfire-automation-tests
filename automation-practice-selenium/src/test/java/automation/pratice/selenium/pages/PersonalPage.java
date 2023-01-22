44745d0bcb0da9737d167165d03a0b317d4b746a
Steffi <steffijoji@gmail.com>
2023-01-22 03:50:20 +0044

    Testes navegando em outras páginas

--- automation-practice-selenium/src/test/java/automation/pratice/selenium/pages/PersonalPage.java
+++ automation-practice-selenium/src/test/java/automation/pratice/selenium/pages/PersonalPage.java
@@ -2,8 +2,21 @@ package automation.pratice.selenium.pages;
 
 import org.openqa.selenium.By;
 
-public class PersonalPage {
+public class PersonalPage extends BasePage {
 
     //Locators
     private By menuPersonalLocator = By.cssSelector("#LinkHeader2");
+    private By titlePersonalLocator = By.cssSelector("body > table > tbody > tr:nth-child(2) > td:nth-child(2) > div.fl > h1");
+
+    public void viewPersonalPage() {
+        if(super.isDisplayed(menuPersonalLocator)) {
+            super.click(menuPersonalLocator);
+        } else {
+            System.out.println("Página não encontrada");
+        }
+    }
+
+    public String getTitlePage() {
+        return super.getText(titlePersonalLocator);
+    }
 }
--- automation-practice-selenium/src/test/java/automation/pratice/selenium/tests/PersonalPageTest.java
+++ automation-practice-selenium/src/test/java/automation/pratice/selenium/tests/PersonalPageTest.java
@@ -1,4 +1,34 @@
 package automation.pratice.selenium.tests;
 
+import automation.pratice.selenium.pages.PersonalPage;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
 public class PersonalPageTest {
+
+    private PersonalPage personalPage;
+    private final String URL = "http://demo.testfire.net/login.jsp";
+
+    @BeforeEach
+    public void setUp() {
+        this.personalPage = new PersonalPage();
+        this.personalPage.visit(URL);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        this.personalPage.quitWebDriver();
+    }
+
+    @Test
+    public void accessPersonalPage() {
+        //when
+        this.personalPage.viewPersonalPage();
+        //then
+        Assertions.assertEquals("Personal", this.personalPage.getTitlePage());
+        Assertions.assertFalse(this.URL.equals(personalPage.getCurrentUrl()));
+    }
 }
